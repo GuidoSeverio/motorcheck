a0ea4ddece337a7c58ecd06bc50ec123b260b3f9
GuidoSeverio <guido.severio@homtail.com>
2023-08-22 12:15:38 -0300

    proyecto

---
+++ motorcheck.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 22-08-2023 a las 15:14:49
+-- Versión del servidor: 5.5.24-log
+-- Versión de PHP: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `motorcheck`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
+(1, 'guido', '81dc9bdb52d04dc20036dbd8313ed055'),
+(2, 'flavia', '81dc9bdb52d04dc20036dbd8313ed055');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -8,14 +8,21 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.post('/', async (req, res, next) =>{
+router.get('/logout', function(req, res, next) {
+	req.session.destroy();
+	res.render('admin/login', {
+		layout: 'admin/layout'
+	});
+});
+
+router.post('/', async (req, res, next) => {
 	try{
 		var usuario = req.body.usuario;
 		var password = req.body.password;
 
-		var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+		var data = await usuariosModel.getUserByUsernameAndPassword (usuario, password);
 
-		if (data != undefined){
+		if (data != undefined) {
 			req.session.id_usuario = data.id;
 			req.session.nombre = data.usuario;
 			res.redirect('/admin/novedades');
@@ -29,13 +36,5 @@ router.post('/', async (req, res, next) =>{
 		console.log(error);
 	}
 });
-
-router.get('/logout', function(req, res, next) {
-	req.session.destroy();
-	res.render('admin/login', {
-		layout: 'admin/layout'
-	});
-});
-
 
 module.exports = router;
